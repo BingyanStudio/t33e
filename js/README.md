af0d5c63c14e680b1feedf68ee14f481baa27fff
Ji Zihao <zhao11fs@gmail.com>
2016-03-17 01:09:32 +0800

    add get length with emoji function

---
+++ js/README.md
@@ -0,0 +1,5 @@
+# javascript code snippets
+这里包含了一些javascript会用的到一些代码段作为参考
+
+* string_length.js
+ * getLengthWithEmoji 获取带emoji字符串正确的长度
---
+++ js/string_length.js
@@ -0,0 +1,13 @@
+/*
+getLength("😄😄") -> 2
+"😄😄" -> 4
+ */
+function getLengthWithEmoji(text) {
+    var emojiRegex = /([\uD800-\uDBFF][\uDC00-\uDFFF])/g;
+    var emojis = text.match(emojiRegex); // find all emoji
+    var noEmojiText = text.replace(emojiRegex, ""); // clear all emoji
+    if (emojis) {
+        return noEmojiText.length + emojis.length;
+    }
+    return text.length;
+}
