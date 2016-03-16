cab20e8924e80817f429bb98020d566f4764c355
Ji Zihao <zhao11fs@gmail.com>
2016-03-17 01:12:11 +0800

    fix typo

--- js/string_length.js
+++ js/string_length.js
@@ -1,11 +1,11 @@
 /*
 getLengthWithEmoji("😄😄") -> 2
-"😄😄" -> 4
+"😄😄".length -> 4
  */
 function getLengthWithEmoji(text) {
     var emojiRegex = /([\uD800-\uDBFF][\uDC00-\uDFFF])/g;
-    var emojis = text.match(emojiRegex); // find all emoji
-    var noEmojiText = text.replace(emojiRegex, ""); // clear all emoji
+    var emojis = text.match(emojiRegex); // find all emojis
+    var noEmojiText = text.replace(emojiRegex, ""); // clear all emojis
     if (emojis) {
         return noEmojiText.length + emojis.length;
     }
