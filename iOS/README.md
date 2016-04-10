68586d48382f823300f9c2e472593c5d51adbc97
Ji Zihao <zhao11fs@gmail.com>
2016-04-10 18:04:11 +0800

    add iOS concat videos

---
+++ iOS/README.md
@@ -0,0 +1,5 @@
+# iOS
+code snippet of iOS
+
+* concatVideos.m 包含iOS连接视频的bug（for循环的版本必须要保存AVURLAsset）
+
---
+++ iOS/concatVideos.m
@@ -0,0 +1,39 @@
+/*
+concat videos and save to saveURL
+must save the AVURLAsset in a array
+ */
+- (void)concatVideos:(NSArray<NSURL *> *)videos saveTo:(NSURL *)saveURL completion:(void(^)(NSError *))completion {
+    // overwrite file
+    NSError *error;
+    [self removeFileAt:saveURL error:&error];
+    
+    // arrays
+    NSMutableArray<AVURLAsset *> *assets = [[NSMutableArray alloc] init];
+    NSMutableArray<NSValue *> *timeRanges = [[NSMutableArray alloc] init];
+    NSMutableArray<AVAssetTrack *> *tracks = [[NSMutableArray alloc] init];
+    
+    for (NSURL *url in videos) {
+        [LULogUtil logWithTag:kLogTag andFormat:@"%@", url];
+        [assets addObject:[AVURLAsset assetWithURL:url]]; // 坑 必须要保存下AVURLAsset
+        [timeRanges addObject:[NSValue valueWithCMTimeRange:[self timeRangeOfAVAsset:[assets lastObject]]]];
+        [tracks addObject:[[[assets lastObject] tracksWithMediaType:AVMediaTypeVideo] firstObject]];
+    }
+    
+    // output
+    AVMutableComposition *composition = [AVMutableComposition composition];
+    AVMutableCompositionTrack *compositionTrack = [composition addMutableTrackWithMediaType:AVMediaTypeVideo preferredTrackID:kCMPersistentTrackID_Invalid];
+    [compositionTrack insertTimeRanges:timeRanges ofTracks:tracks atTime:kCMTimeZero error:&error];
+    [LULogUtil logWithTag:kLogTag andFormat:@"insertTimeRanges error %@", error];
+    
+    // export
+    AVAssetExportSession *exporter = [[AVAssetExportSession alloc] initWithAsset:composition presetName:AVAssetExportPresetHighestQuality];
+    if (self.saveFileType) {
+        exporter.outputFileType = self.saveFileType;
+    } else {
+        exporter.outputFileType = AVFileTypeMPEG4;
+    }
+    exporter.outputURL = saveURL;
+    [exporter exportAsynchronouslyWithCompletionHandler:^{
+        completion(exporter.error);
+    }];
+}
